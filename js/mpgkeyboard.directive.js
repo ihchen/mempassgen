ecd319d82c73f98ca7126eb962c219c8f9c911d0
Imanuel Chen <imanuelhchen@gmail.com>
2017-04-17 18:38:12 -0700

    Added controllers

--- js/mpgkeyboard.directive.js
+++ js/mpgkeyboard.directive.js
@@ -1,12 +1,101 @@
-angular.module('MemPassGen', [])
-.directive('mpgKeyboard', function() {
-	function link(scope, element, attrs) {
+angular.module('MemPassGen')
+.directive('mpgKeyboard', function(FingerKeyMap) {
+	function controller() {
+		var self = this;
 
+		this.fingerMap = FingerKeyMap.cloneFingerMap(FingerKeyMap.DEFAULT_FINGER_MAP);
+		this.currentKey;	//Contains the jqlite element of the key that has been selected
+
+		this.updateFingerMap = function(newfinger) {
+			var oldfinger = this.currentKey.attr('finger');
+			var text = this.currentKey.text();
+			var shiftChar = text[0];
+			var notShiftChar = text[1];
+			var letter = false;
+			// If not dual key, then it's a letter
+			if(!text[1]) {
+				notShiftChar = shiftChar.toLowerCase();
+				letter = true;
+			}
+
+			// Change key color
+			this.currentKey.removeClass(oldfinger.slice(1, oldfinger.length));
+			this.currentKey.addClass(newfinger.slice(1, newfinger.length));
+			// Update attribute
+			this.currentKey.attr('finger', newfinger);
+
+			if(letter) {
+				var shiftChars = this.fingerMap[oldfinger].shiftOn.chars;
+				var notShiftChars = this.fingerMap[oldfinger].shiftOff.chars;
+				
+				// Remove
+				shiftChars.splice(shiftChars.indexOf(shiftChar), 1);
+				notShiftChars.splice(notShiftChars.indexOf(notShiftChar), 1);
+				// Add
+				this.fingerMap[newfinger].shiftOn.chars.push(shiftChar);
+				this.fingerMap[newfinger].shiftOff.chars.push(notShiftChar);
+			}
+			else {
+				var shiftSymbols = this.fingerMap[oldfinger].shiftOn.symbols;
+				var notShiftSymbols = this.fingerMap[oldfinger].shiftOff.symbols;
+				var numbers = false;
+				if(notShiftChar.charCodeAt() >= 48 && notShiftChar.charCodeAt() <= 57) {
+					notShiftSymbols = this.fingerMap[oldfinger].shiftOff.numbers;
+					numbers = true;
+				}
+
+				// Remove
+				shiftSymbols.splice(shiftSymbols.indexOf(shiftChar), 1);
+				notShiftSymbols.splice(notShiftSymbols.indexOf(notShiftChar), 1);
+				// Add
+				this.fingerMap[newfinger].shiftOn.symbols.push(shiftChar);
+				if(numbers)
+					this.fingerMap[newfinger].shiftOn.numbers.push(notShiftChar);
+				else
+					this.fingerMap[newfinger].shiftOn.symbols.push(notShiftChar);
+			}
+
+			console.log(this.fingerMap);
+		}
 	}
+
+	function link(scope, element, attrs) {
+		var keys = element.find('mpg-key');
+		var menu = document.querySelector('.menu');
+
+		var onKeysClick = function() {
+			menu.firstElementChild.style.display = "none";	//init-menu
+			menu.lastElementChild.style.display = "block";	//real-menu
+			keys.off('mouseup');
+		}
+		keys.on('mouseup', onKeysClick);
+	}
+
 	return {
 		restrict: 'E',
 		scope: {},
-		templateUrl: '../templ/mpgKeyboard.tmpl.html',
+		templateUrl: '../templates/mpgKeyboard.tmpl.html',
+		controller: controller,
+		controllerAs: 'kbc',
 		link: link
 	};
-});
+})
+.directive('mpgKey', function() {
+	function link(scope, element, attrs, ctrls) {
+		scope.fingerClass = attrs.finger.slice(1, attrs.finger.length);
+
+		var keyboard = ctrls[0];
+		var onKeyClick = function() {
+			keyboard.currentKey = element;
+			scope.$apply();
+		}
+		element.on('click', onKeyClick);
+	}
+
+	return {
+		restrict: 'AE',
+		scope: {},
+		require: ['^mpgKeyboard'],
+		link: link
+	}
+});	
